d114b5b161b4d3487594f91fd1e0b74d53c8bed4
VBerko <vera.berko@gmail.com>
2014-11-24 23:36:27 +0200

    2.1

---
+++ HW01_2.1/HW01_2.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HW01_2._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            float operand1 = 123;
+            short operand2 = 0;
+            string sign;
+
+            Console.WriteLine("\nChoose action / * - + ");
+            sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "/":
+                    if (operand2 != 0)
+                    {
+                        Console.WriteLine("{0} {1} {2} = {3}", operand1, sign, operand2, operand1 / operand2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Dividing by zero. Impossible to make action");
+                    }
+                    break;
+                case "*":
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, sign, operand2, operand1 * operand2);
+                    break;
+                case "-":
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, sign, operand2, operand1 - operand2);
+                    break;
+                case "+":
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, sign, operand2, operand1 + operand2);
+                    break;
+                default:
+                    Console.WriteLine("Error: Incorrect value");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
